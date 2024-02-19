8c751ad2871506b96b4a60ebe022b3162ff7e2e7
TNLISN <nicolas45lav@gmail.com>
2024-02-19 13:17:35 +0100

    Add query

--- 2_analyze/query.py
+++ 2_analyze/query.py
@@ -5,16 +5,24 @@ import os
 load_dotenv()
 
 class OpenAIQuery:
-    def __init__(self):
+    def __init__(self, query_file='../data/query.txt'):
         self.api_key = os.getenv("OPENAI_API_KEY")
         openai.api_key = self.api_key
+        self.query = self.load_query_from_file(query_file)
+
+    def load_query_from_file(self, filepath):
+        try:
+            with open(filepath, 'r', encoding='utf-8') as file:
+                return file.read().strip()
+        except Exception as e:
+            return f"Error loading query from {filepath}: {e}"
 
     def query_gpt(self, row):
         try:
             completion = openai.ChatCompletion.create(
                 model="gpt-4-0125-preview",
                 messages=[
-                    {"role": "system", "content": query},
+                    {"role": "system", "content": self.query},
                     {"role": "user", "content": row},
                 ],
             )
